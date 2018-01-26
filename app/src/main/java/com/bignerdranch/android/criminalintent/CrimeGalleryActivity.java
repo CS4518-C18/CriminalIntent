337e092cb495e85cd9a0401bb0544215d4555baa
Haofan Zhang <hzhang3@wpi.edu>
2018-01-26 17:01:05 -0500

    grid view, activity pass

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeGalleryActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_gallery);
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/ImageAdapter.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by haofanzhang on 1/26/18.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+}
